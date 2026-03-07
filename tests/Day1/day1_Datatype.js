3d219f9d8b82e9d572c900f11ad9d8236f8d638c
Bharani <bharanidharan.k31@gmail.com>
2026-03-07 14:30:41 +0530

    Initial Playwright project setup

---
+++ tests/Day1/day1_Datatype.js
@@ -0,0 +1,11 @@
+var firstName = "Bharani";
+var companyName = "XCS";
+var mobileNumber = 9080238880;
+var isAutomation = true;
+var hasPlaywright;
+
+console.log("First Name is " + firstName + " and datatype is " + typeof(firstName));
+console.log("Company Name is " + companyName + " and datatype is " + typeof(companyName));
+console.log("Mobile Number is " + mobileNumber + " and datatype is " + typeof(mobileNumber));
+console.log("Is Automation is " + isAutomation + " and datatype is " + typeof(isAutomation));
+console.log("Has Playwright is " + hasPlaywright + " and datatype is " + typeof(hasPlaywright));
