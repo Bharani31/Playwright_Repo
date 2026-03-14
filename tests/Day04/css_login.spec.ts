b4bef0f3d415447c8da2aa5f677acd8e9e3753d3
Bharani <bharanidharan.k31@gmail.com>
2026-03-14 12:50:02 +0530

    Changes to be committed: 	modified:   tests/Day04/css_login.spec.ts 	new file:   tests/Day04/css_selector_home.spec.ts

--- tests/Day04/css_login.spec.ts
+++ tests/Day04/css_login.spec.ts
@@ -2,13 +2,13 @@ import { test } from "@playwright/test";
 
 test("To Launch a Browser", async({ page }) => {
 
-    await page.goto("https://testleaf.my.salesforce.com/");
+    await page.goto("http://leaftaps.com/opentaps/control/main");
 
     await page.locator(`#username`).fill("dilipkumar.rajendran@testleaf.com")
 
     await page.locator(`#password`).fill("TestLeaf@2025")
 
-    await page.locator(`#Login`).click()
+    await page.locator(`.decorativeSubmit`).click()
 
     await page.waitForTimeout(3000)
 
